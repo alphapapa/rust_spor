8766152d339abcf4362068dd709b8c9644c2fd48
Austin Bingham <austin.bingham@gmail.com>
2018-09-23 21:47:33 +0200

    Trying new stuff.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 extern crate ndarray;
 extern crate ordered_float;
 
+use std::collections::HashMap;
+
 pub mod scoring;
 
 // TODO: Consider using the seal library for smith-waterman. Once we learn how to do it ourselves...
@@ -8,34 +10,87 @@ pub mod scoring;
 type ScoringFunction = Fn(char, char) -> f32;
 type GapPenaltyFunction = Fn(u32) -> f32;
 
-#[derive(Debug)]
-enum Direction {
-    NONE,
-    DIAG,
-    UP,
-    LEFT,
+#[derive(Clone, Debug, Eq, PartialEq, Hash)]
+pub enum Direction {
+    Diag,
+    Up,
+    Left
 }
 
-fn direction_value(direction: &Direction) -> u8 {
-    match direction {
-        Direction::NONE => 0x00,
-        Direction::DIAG => 0x01,
-        Direction::UP => 0x02,
-        Direction::LEFT => 0x04,
-    }
+#[derive(Debug)]
+pub enum Directions {
+    None,
+    Some(Vec<Direction>)
 }
+
+type ScoreMatrix = ndarray::Array2<f32>;
+type TracebackMatrix = ndarray::Array2<Directions>;
+
+fn _tracebacks<T>(score_matrix: &ScoreMatrix,
+                  traceback_matrix: &TracebackMatrix,
+                  idx: T) -> [u32;0]
+    where T: ndarray::NdIndex<ndarray::Ix2> 
+{
+    let score = score_matrix[idx];
+    if score == 0.0 {
+        return []
+    }
+
+    // let directions = traceback_matrix[idx];
+    // assert!(direction_value(&directions) != direction_value(Direction::NONE),
+    //        "Tracebacks with direction NONE should have value 0!");
+    []
+
+//     score = score_matrix[idx]
+//     if score == 0:
+//         yield ()
+//         return
+
+//     directions = traceback_matrix[idx]
+
+//     assert directions != Direction.NONE, 'Tracebacks with direction NONE should have value 0!'
+
+//     row, col = idx
+
+//     if directions & Direction.UP.value:
+//         for tb in _tracebacks(score_matrix, traceback_matrix, (row - 1, col)):
+//             yield itertools.chain(tb, ((idx, Direction.UP),))
+
+//     if directions & Direction.LEFT.value:
+//         for tb in _tracebacks(score_matrix, traceback_matrix, (row, col - 1)):
+//             yield itertools.chain(tb, ((idx, Direction.LEFT),))
+
+//     if directions & Direction.DIAG.value:
+//         for tb in _tracebacks(score_matrix, traceback_matrix, (row - 1, col - 1)):
+//             yield itertools.chain(tb, ((idx, Direction.DIAG),))
+}
+
+// def tracebacks(score_matrix, traceback_matrix, idx):
+// """Calculate the tracebacks for `traceback_matrix` starting at index `idx`.
+
+//     Returns: An iterable of tracebacks where each traceback is sequence of
+//       (index, direction) tuples. Each `index` is an index into
+//       `traceback_matrix`. `direction` indicates the direction into which the
+//       traceback "entered" the index.
+//     """
+//     return filter(lambda tb: tb != (),
+//                   _tracebacks(score_matrix,
+//                               traceback_matrix,
+//                               idx))
 
 pub fn build_score_matrix(
     a: &str,
     b: &str,
     score_func: &ScoringFunction,
     gap_penalty: &GapPenaltyFunction)
-    -> (ndarray::Array2<f32>,
-        ndarray::Array2<u8>) {
-    let mut score_matrix = ndarray::Array2::<f32>::zeros(
-        (a.len() + 1, b.len() + 1));
-    let mut traceback_matrix = ndarray::Array2::<u8>::zeros(
-        (a.len() + 1, b.len() + 1));
+    -> (ScoreMatrix,
+        TracebackMatrix) {
+        let mut score_matrix = ScoreMatrix::zeros(
+            (a.len() + 1, b.len() + 1));
+
+        let mut traceback_matrix = TracebackMatrix::from_shape_fn(
+            (a.len() + 1, b.len() + 1),
+            |n| Directions::None);
 
     for (row, a_char) in a.chars().enumerate() {
         for (col, b_char) in b.chars().enumerate() {
@@ -43,26 +98,36 @@ pub fn build_score_matrix(
             let col = col + 1;
             let match_score = score_func(a_char, b_char);
 
-            let mut scores = [
-                (score_matrix[(row - 1, col - 1)] + match_score,
-                 Direction::DIAG),
-                (score_matrix[(row - 1, col)] - gap_penalty(1),
-                 Direction::UP),
-                (score_matrix[(row, col - 1)] - gap_penalty(1),
-                 Direction::LEFT),
-                (0.0, Direction::NONE)
-            ];
-            scores.sort_by_key(|k| ordered_float::OrderedFloat(k.0));
-            scores.reverse();
+            let mut scores: HashMap<Direction, f32> = HashMap::new();
+            scores.insert(Direction::Diag,
+                          score_matrix[(row - 1, col - 1)] + match_score);
+            scores.insert(Direction::Up,
+                          score_matrix[(row - 1, col)] - gap_penalty(1));
+            scores.insert(Direction::Left,
+                          score_matrix[(row, col - 1)] - gap_penalty(1));
 
-            let max_score = scores[0].0;
+            let mut max_score = std::f32::MIN;
+            let mut diags: Vec<Direction> = vec![];
 
-            let scores = scores.iter().take_while(|n| n.0 == max_score);
+            for (direction, &score) in &scores {
+                if score < max_score {
+                    continue;
+                }
 
-            score_matrix[(row, col)] = max_score;
+                if score > max_score {
+                    diags.clear();
+                    max_score = score;
+                }
 
-            for (_, direction) in scores {
-                traceback_matrix[(row, col)] = traceback_matrix[(row, col)] | direction_value(direction);
+                diags.push(direction.clone());
+            }
+
+            if max_score > 0.0 {
+                score_matrix[(row, col)] = max_score;
+
+                if !diags.is_empty() {
+                    traceback_matrix[(row, col)] = Directions::Some(diags);
+                }
             }
         }
     }
--- tests/canned_tests.rs
+++ tests/canned_tests.rs
@@ -6,6 +6,41 @@ use spor::scoring::*;
 
 const INPUT1: &str = "GGTTGACTA";
 const INPUT2: &str = "TGTTACGG";
+
+#[test]
+fn canned_tracebacks() {
+    // let (score_matrix, _) = build_score_matrix(
+    //     INPUT1, INPUT2, &score_func, &gap_penalty);
+
+    // let mut max_idx = (0, 0);
+    // let mut max_score = std::f32::MIN;
+
+    // // TODO: Is there a way to lazily enumerate the array? We rolls our own for now...
+    // for row in 0..score_matrix.dim().0 {
+    //     for col in 0..score_matrix.dim().1 {
+    //         let score = score_matrix[(row, col)];
+    //         if score > max_score {
+    //             max_score = score;
+    //             max_idx = (row, col);
+    //         }
+    //     }
+    // }
+
+//     tbs = list(tracebacks(score_matrix, traceback_matrix, max_idx))
+//     assert len(tbs) == 1
+
+//     expected = (
+//         ((2, 2), Direction.DIAG),
+//         ((3, 3), Direction.DIAG),
+//         ((4, 4), Direction.DIAG),
+//         ((5, 4), Direction.UP),
+//         ((6, 5), Direction.DIAG),
+//         ((7, 6), Direction.DIAG))
+
+//     assert tuple(tbs[0]) == expected
+}
+
+
 #[test]  
 fn canned_score_matrix() {
     let expected = ndarray::Array::from_shape_vec(
@@ -22,7 +57,15 @@ fn canned_score_matrix() {
              0, 1, 0, 3, 2, 7, 9, 8, 7].iter().map(|n| {*n as f32}).collect()).unwrap();
     
     let (score_matrix, _) = build_score_matrix(
-        input1, input2, &score_func, &gap_penalty);
+        INPUT1, INPUT2, &score_func, &gap_penalty);
 
     assert_eq!(expected, score_matrix);
 }
+
+// def test_canned_alignment():
+// _, alignments = align(ROWS, COLS, score, gap_penalty)
+//     alignments = list(alignments)
+//     assert len(alignments) == 1
+//     actual = tuple(alignments[0])
+//     expected = ((1, 1), (2, 2), (3, 3), (4, None), (5, 4), (6, 5))
+//     assert actual == expected
