eafb25292263a6050de2e18204c20fba8d2c3424
Austin Bingham <austin.bingham@gmail.com>
2018-09-25 11:22:34 +0200

    Using cute comprehensions crate.

--- src/lib.rs
+++ src/lib.rs
@@ -3,33 +3,34 @@ pub mod anchor;
 pub mod scoring;
 
 #[macro_use]
+extern crate cute;
 extern crate cpython;
 
 use alignment::smith_waterman::*;
 use cpython::*;
 
-fn align(py: Python, a: String, b: String) -> PyResult<PyList> {
-    let (_score, alignments) = alignment::smith_waterman::align(
+fn align(py: Python, a: String, b: String) -> PyResult<PyTuple> {
+    let (score, alignments) = alignment::smith_waterman::align(
         &*a,
         &*b,
         &scoring::score_func,
         &scoring::gap_penalty);
 
-    let mut py_alignments = vec![];
-    for alignment in alignments {
-        let mut py_alignment = vec![];
-        for cell in alignment {
-            let py_cell = match cell {
+    let alignments = c![
+        c![
+            match cell {
                 AlignmentCell::Both {left, right} => (Some(left), Some(right)),
                 AlignmentCell::RightGap {left} => (Some(left), None),
                 AlignmentCell::LeftGap {right} => (None, Some(right)),
-            };
-            py_alignment.push(py_cell);
-        }
-        py_alignments.push(py_alignment);
-    }
+            },
+            for cell in alignment
+        ],
+        for alignment in alignments
+    ];
 
-    Ok(py_alignments.to_py_object(py))
+    let result = (score, alignments);
+
+    Ok(result.to_py_object(py))
 }
 
 py_module_initializer!(rust_spor, initstatus, PyInit_rust_spor, |py, m| {
