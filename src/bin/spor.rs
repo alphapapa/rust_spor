3c783bbe3dc8c3124dadd6d06a3b2bbec9df3db8
Austin Bingham <austin.bingham@gmail.com>
2019-03-14 12:59:45 +0100

    Diff handler now works with id prefixes.

--- src/bin/spor.rs
+++ src/bin/spor.rs
@@ -1,4 +1,7 @@
-extern crate failure; 
+extern crate failure;
+
+#[macro_use]
+extern crate log;
 
 #[macro_use]
 extern crate serde_derive;
@@ -6,6 +9,7 @@ extern crate serde_derive;
 extern crate docopt;
 extern crate exit_code;
 extern crate serde_yaml;
+extern crate simple_logger;
 extern crate spor;
 
 use std::path::PathBuf;
@@ -54,37 +58,32 @@ struct Args {
 type CommandResult = std::result::Result<(), i32>;
 
 fn init_handler() -> CommandResult {
-    let path = std::env::current_dir()
-        .map_err(|_| exit_code::OS_FILE_ERROR)?;
+    let path = std::env::current_dir().map_err(|_| exit_code::OS_FILE_ERROR)?;
 
-    spor::repository::initialize(&path, None) 
-        .map_err(|_| exit_code::DATA_ERROR)?;
+    spor::repository::initialize(&path, None).map_err(|_| exit_code::DATA_ERROR)?;
 
     Ok(())
 }
 
 fn open_repo(path: &PathBuf) -> std::result::Result<Repository, i32> {
-    Repository::new(&path, None)
-        .map_err(|e| {
-            println!("{:?}", e);
-            exit_code::OS_FILE_ERROR
-        })
+    Repository::new(&path, None).map_err(|e| {
+        println!("{:?}", e);
+        exit_code::OS_FILE_ERROR
+    })
 }
 
 fn add_handler(args: &Args) -> CommandResult {
-    let path = std::env::current_dir()
-        .map_err(|e| {
-            println!("{:?}", e);
-            exit_code::OS_FILE_ERROR
-        })?;
+    let path = std::env::current_dir().map_err(|e| {
+        println!("{:?}", e);
+        exit_code::OS_FILE_ERROR
+    })?;
 
     let repo = open_repo(&path)?;
 
-    let metadata = serde_yaml::from_reader(std::io::stdin())
-        .map_err(|e| {
-            println!("{:?}", e);
-            exit_code::DATA_ERROR
-        })?;
+    let metadata = serde_yaml::from_reader(std::io::stdin()).map_err(|e| {
+        println!("{:?}", e);
+        exit_code::DATA_ERROR
+    })?;
 
     let encoding = "utf-8".to_string();
 
@@ -100,8 +99,9 @@ fn add_handler(args: &Args) -> CommandResult {
             c,
             metadata,
             encoding,
-        )}
-    ).map_err(|e| {
+        )
+    })
+    .map_err(|e| {
         println!("{:?}", e);
         exit_code::DATA_ERROR
     })?;
@@ -118,11 +118,13 @@ fn list_handler(args: &Args) -> CommandResult {
     let file = std::path::Path::new(&args.arg_source_file);
     let repo = open_repo(&file.to_path_buf())?;
     for (id, anchor) in &repo {
-        println!("{} {:?}:{} => {:?}",
+        println!(
+            "{} {:?}:{} => {:?}",
             id,
             anchor.file_path(),
             anchor.context().offset(),
-            anchor.metadata());
+            anchor.metadata()
+        );
     }
 
     Ok(())
@@ -133,15 +135,16 @@ fn status_handler(_args: &Args) -> CommandResult {
     let repo = open_repo(&file.to_path_buf())?;
 
     for (id, anchor) in &repo {
-        let (changed, diffs) = get_anchor_diff(&anchor)
-            .map_err(|_e| exit_code::OS_FILE_ERROR)?;
+        let (changed, diffs) = get_anchor_diff(&anchor).map_err(|_e| exit_code::OS_FILE_ERROR)?;
 
         if changed {
             println!("{:?}", diffs);
-            println!("{} {}:{} out-of-date", 
-                     id, 
-                     anchor.file_path().to_string_lossy(), 
-                     anchor.context().offset());
+            println!(
+                "{} {}:{} out-of-date",
+                id,
+                anchor.file_path().to_string_lossy(),
+                anchor.context().offset()
+            );
         }
     }
 
@@ -152,16 +155,8 @@ fn diff_handler(args: &Args) -> CommandResult {
     let file = std::path::Path::new(".");
     let repo = open_repo(&file.to_path_buf())?;
 
-    let anchor = repo.get(&args.arg_anchor_id)
-        .or(Err(exit_code::OS_FILE_ERROR))
-        .map(|a| match a {
-            Some(anchor) => Ok(anchor),
-            None => Err(exit_code::OS_FILE_ERROR)
-        })
-        .unwrap_or(Err(exit_code::OS_FILE_ERROR))?;
-
-    let (_changed, diff) = get_anchor_diff(&anchor)
-        .map_err(|_| exit_code::OS_FILE_ERROR)?;
+    let (_, anchor) = get_anchor(&repo, &args.arg_anchor_id)?;
+    let (_changed, diff) = get_anchor_diff(&anchor).map_err(|_| exit_code::OS_FILE_ERROR)?;
 
     for line in diff {
         println!("{}", line);
@@ -173,39 +168,41 @@ fn diff_handler(args: &Args) -> CommandResult {
 fn update_handler(_args: &Args) -> CommandResult {
     let file = std::path::Path::new(".");
 
-    let repo = Repository::new(file, None)
-        .map_err(|_| exit_code::OS_FILE_ERROR)?;
+    let repo = Repository::new(file, None).map_err(|_| exit_code::OS_FILE_ERROR)?;
 
     for (id, anchor) in &repo {
-        let updated = update(&anchor, &align)
-            .map_err(|e| {
-                println!("{:?}", e);
-                exit_code::DATA_ERROR})?;
+        let updated = update(&anchor, &align).map_err(|e| {
+            println!("{:?}", e);
+            exit_code::DATA_ERROR
+        })?;
 
-        repo.update(id, &updated)
-            .map_err(|e| {
-                println!("{:?}", e);
-                exit_code::OS_FILE_ERROR})?;
-    };
+        repo.update(id, &updated).map_err(|e| {
+            println!("{:?}", e);
+            exit_code::OS_FILE_ERROR
+        })?;
+    }
 
     Ok(())
 }
 
+/// Find an anchor based on a prefix of its ID.
+/// If there is not exactly one match for the ID prefix, then this returns an error.
 fn get_anchor(repo: &Repository, id_prefix: &str) -> std::result::Result<(AnchorId, Anchor), i32> {
-    let mut prefixed: Vec<(AnchorId, Anchor)> = repo.into_iter()
+    let mut prefixed: Vec<(AnchorId, Anchor)> = repo
+        .into_iter()
         .filter(|(id, _anchor)| id.starts_with(id_prefix))
         .collect();
 
     if prefixed.len() > 1 {
-        println!("Ambiguous ID specification");
-        return Err(exit_code::DATA_ERROR)
+        error!("Ambiguous ID specification: {}", id_prefix);
+        return Err(exit_code::DATA_ERROR);
     }
 
     match prefixed.pop() {
         Some(m) => return Ok(m),
         None => {
-            println!("No anchor matching ID specification");
-            return Err(exit_code::DATA_ERROR)
+            error!("No anchor matching ID specification: {}", id_prefix);
+            return Err(exit_code::DATA_ERROR);
         }
     }
 }
@@ -216,7 +213,8 @@ fn details_handler(args: &Args) -> CommandResult {
 
     let (id, anchor) = get_anchor(&repo, &args.arg_id)?;
 
-    print!("id: {}
+    print!(
+        "id: {}
 path: {:?}
 encoding: {}
 
@@ -233,27 +231,29 @@ encoding: {}
 --------
 
 offset: {}
-width: {}", 
-    id, 
-    anchor.file_path(), 
-    anchor.encoding(),
-    anchor.context().before(),
-    anchor.context().topic(),
-    anchor.context().after(),
-    anchor.context().offset(),
-    anchor.context().width(),
+width: {}",
+        id,
+        anchor.file_path(),
+        anchor.encoding(),
+        anchor.context().before(),
+        anchor.context().topic(),
+        anchor.context().after(),
+        anchor.context().offset(),
+        anchor.context().width(),
     );
 
     Ok(())
 }
 
 fn main() {
+    simple_logger::init().unwrap();
+
     let args: Args = Docopt::new(USAGE)
         .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
 
     let result = if args.cmd_init {
-        init_handler() 
+        init_handler()
     } else if args.cmd_list {
         list_handler(&args)
     } else if args.cmd_status {
@@ -270,9 +270,8 @@ fn main() {
         Err(exit_code::FAILURE)
     };
 
-    std::process::exit(
-        match result {
-            Ok(_) => exit_code::SUCCESS,
-            Err(code) => code
-        });
+    std::process::exit(match result {
+        Ok(_) => exit_code::SUCCESS,
+        Err(code) => code,
+    });
 }
